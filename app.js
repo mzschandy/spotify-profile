6f0ae7cefb5224e6d116c852e3df8c4aa6717305
Zach SL <mzschandy@gmail.com>
2021-12-21 18:25:11 -0500

    heroku setup

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 const express = require("express")
 const path = require("path")
 const bodyParser = require("body-parser")
@@ -7,16 +9,22 @@ const querystring = require("query-string")
 const cookieParser = require('cookie-parser');
 
 const app = express() 
-const port = 5000
+const PORT = process.env.PORT || process.env.PORT || 5000
 
 app.use(express.static(path.resolve(__dirname, "../client/public/")))
 app.use(cors({origin: true, credentials: true}))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser())
 
-const client_id = "1f75bb9187d04b2a8015552aad3f58de"
-const client_secret = "b41586d0697c4deda5d16325d9599376"
-const redirect_uri = "http://localhost:5000/callback"
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+let REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:5000/callback"
+let FRONTEND_URI = process.env.FRONTEND_URI || "http://localhost:3000"
+
+if (process.env.NODE_ENV !== 'production') {
+  REDIRECT_URI = 'http://localhost:5000/callback';
+  FRONTEND_URI = 'http://localhost:3000';
+}
 
 const generateRandomString = (length) => {
   let text = '';
@@ -30,49 +38,36 @@ const generateRandomString = (length) => {
 
 const stateKey = 'spotify_auth_state';
 
-app.get("/test", (req, res) => {
-  //res.redirect("https://theverge.com");
-  console.log("This is the test route!");
-  res.redirect("/test2");
-
-})
-
 app.get("/", (req, res) => {
   res.render(path.resolve(__dirname, "../client/public/index.html"))
   console.log("This is the default/home route");
-})
-
-app.get("/test2", (req, res) => {
-  console.log("test 2")
 })
 
 app.get("/login", (req, res) => {
   const state = generateRandomString(16)
   const scope = "user-read-private user-read-email user-follow-read user-library-read user-top-read"
 
-  console.log("Oh this is the login route");
+  // console.log("Oh this is the login route");
 
   res.cookie(stateKey, state);
   res.redirect("https://accounts.spotify.com/authorize?" +
   querystring.stringify({
     response_type: 'code',
-    client_id: client_id,
+    client_id: CLIENT_ID,
     scope: scope,
-    redirect_uri: redirect_uri,
+    redirect_uri: REDIRECT_URI,
     state: state
   }))
 })
 
 app.get("/callback", (req, res) => {
-  console.log("Made it to the callback!!");
-
   const code = req.query.code || null
   const state = req.query.state || null
   const storedState = req.cookies ? req.cookies[stateKey] : null
 
-  console.log("code>", code);
-  console.log("state>", state);
-  console.log("stored state>", storedState);
+  // console.log("code>", code);
+  // console.log("state>", state);
+  // console.log("stored state>", storedState);
 
   if (state === null || state !== storedState) {
     console.log("RIP something went wrong");
@@ -81,7 +76,7 @@ app.get("/callback", (req, res) => {
       error: "state_mismatch"
     }))
   } else {
-    console.log("Things are going well");
+    // console.log("Things are going well");
 
     res.clearCookie(stateKey)
 
@@ -89,23 +84,23 @@ app.get("/callback", (req, res) => {
       url: 'https://accounts.spotify.com/api/token',
       form: {
         code: code,
-        redirect_uri: redirect_uri,
+        redirect_uri: REDIRECT_URI,
         grant_type: 'authorization_code'
       },
       headers: {
-        'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+        'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
       },
       json: true,
     }
 
-    console.log("authOptions.json > ", authOptions.json);
-    console.log("authOptions", authOptions)
+    // console.log("authOptions.json > ", authOptions.json);
+    // console.log("authOptions", authOptions)
 
 
     request.post(authOptions, (error, response, body) => {
-      console.log("made it to the request function")
+      // console.log("made it to the request function")
       if(!error && response.statusCode === 200) {
-        console.log("body", body);
+        // console.log("body", body);
 
         const access_token = body.access_token
         const refresh_token = body.refresh_token
@@ -120,17 +115,17 @@ app.get("/callback", (req, res) => {
         request.get(options, (error, response, body) => {
           console.log(body)
         })*/
-        console.log(access_token);
-        console.log(refresh_token);
+        // console.log(access_token);
+        // console.log(refresh_token);
 
-        res.redirect("http://localhost:3000/#" + 
+        res.redirect(`${FRONTEND_URI}/#` + 
         querystring.stringify({
           access_token,
           refresh_token
         }))
 
-        console.log(access_token);
-        console.log(refresh_token);
+        // console.log(access_token);
+        // console.log(refresh_token);
       } else {
         res.redirect("/#" +
         querystring.stringify({
@@ -145,7 +140,7 @@ app.get("/refresh_token", (req, res) => {
   const refresh_token = req.query.refresh_token
   const authOptions = {
     url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')) },
+    headers: { 'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')) },
     form: {
       grant_type: 'refresh_token',
       refresh_token: refresh_token
@@ -168,6 +163,6 @@ app.get("*", (req, res) => {
 })
 
 
-app.listen(port, () => {
-  console.log("App running on port", port)
+app.listen(PORT, () => {
+  console.log("App running on port", PORT)
 })
