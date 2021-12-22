05a4371b352d59590c6b76976b1f89a9b321eb20
Zach SL <mzschandy@gmail.com>
2021-12-22 01:46:46 -0500

    heroku

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const querystring = require("query-string")
 const cookieParser = require('cookie-parser');
 
 const app = express() 
-const PORT = process.env.PORT || process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.resolve(__dirname, "./client/build")))
+app.use(express.static(path.resolve(__dirname, "/client/build")))
 app.use(cors({origin: true, credentials: true}))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser())
@@ -39,7 +39,7 @@ const generateRandomString = (length) => {
 const stateKey = 'spotify_auth_state';
 
 app.get("/", (req, res) => {
-  res.render(path.resolve(__dirname, "./client/build/index.html"))
+  res.render(path.resolve(__dirname, "/client/build/index.html"))
   // console.log("This is the default/home route");
 })
 
@@ -159,7 +159,7 @@ app.get("/refresh_token", (req, res) => {
 })
 
 app.get("*", (req, res) => {
-  res.render(path.resolve(__dirname, "./client/build/index.html"))
+  res.render(path.resolve(__dirname, "/client/build/index.html"))
 })
 
 
