8ab5f19b7fc74fe13c21aec970825c9170c61ec3
Zach SL <mzschandy@gmail.com>
2021-12-21 19:05:28 -0500

    another tailwind fix?

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
