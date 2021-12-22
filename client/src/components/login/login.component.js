052e83ca334de88148e0665036598d81c796092d
Zach SL <mzschandy@gmail.com>
2021-12-22 16:29:20 -0500

    hardcoded login url to href

--- client/src/components/login/login.component.js
+++ client/src/components/login/login.component.js
@@ -10,7 +10,8 @@ const Login = () => {
       <div className="w-screen h-screen flex">
         <div className="justify-center self-center mx-auto">
           <div className="text-4xl font-semibold mb-4">Spotify Profile</div>
-          <a className="px-4 py-2 bg-green-700 rounded-full w-52 h-14 flex text-center justify-center items-center mx-auto" href={LOGIN_URI}>
+          <a className="px-4 py-2 bg-green-700 rounded-full w-52 h-14 flex text-center justify-center items-center mx-auto" 
+            href="https://spotify-profile-card.herokuapp.com/login">
             <IconContext.Provider value={{size: "2em"}}>
               <span>
                 <FaSpotify />
