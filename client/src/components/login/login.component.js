8d5dcd8ab92ad4c914761be04d47f2f9491a40c7
Zach SL <mzschandy@gmail.com>
2021-12-22 16:25:44 -0500

    hardcoded login uri

--- client/src/components/login/login.component.js
+++ client/src/components/login/login.component.js
@@ -2,9 +2,7 @@ import React from "react"
 import {FaSpotify} from "react-icons/fa"
 import { IconContext } from "react-icons"
 
-const LOGIN_URI = process.env.NODE_ENV !== 'production'
-? 'http://localhost:5000/login'
-: 'https://spotify-profile-card.herokuapp.com/login';
+const LOGIN_URI =  'https://spotify-profile-card.herokuapp.com/login';
 
 const Login = () => {
   return (
